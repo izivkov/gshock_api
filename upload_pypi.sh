82eaa5371c625a3bfb7bce44824f2b96e010be3c
Just Me <izivkov@gmail.com>
2025-08-07 23:04:57 -0400

    Added support for ABL watches

--- upload_pypi.sh
+++ upload_pypi.sh
@@ -1,2 +1,2 @@
 python3 -m pip install --upgrade twine
-python3 -m twine upload dist/gshock_api-2.0.21-py3-none-any.whl dist/gshock_api-2.0.21.tar.gz
+python3 -m twine upload dist/gshock_api-2.0.22-py3-none-any.whl dist/gshock_api-2.0.22.tar.gz
