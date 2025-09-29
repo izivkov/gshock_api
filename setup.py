26cb4b95041d3f96cd3676d524e3e66cee52b964
Ivo Zivkov <izivkov@gmail.com>
2025-09-28 23:31:43 -0400

    Now connects based on UUID instead of name

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gshock_api',
-    version='2.0.26',
+    version='2.0.27',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     install_requires=[
