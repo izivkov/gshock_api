51c4c0a94632fc55c8d771a6e90b2cb53387a61b
Ivo Zivkov <izivkov@gmail.com>
2025-11-25 12:26:27 -0500

    Use uv to build

--- upload-pypi-uv.sh
+++ upload-pypi-uv.sh
@@ -1,5 +1,11 @@
-rm -rf dist
-uv build
-twine upload dist/*
-uv publish
+#!/bin/bash
+set -e
 
+# Clean previous builds
+rm -rf dist
+
+# Build distributions with uv
+uv build
+
+# Upload distributions using twine within uv environment
+uv run twine upload dist/*
--- upload-pypi.sh
+++ upload-pypi.sh
@@ -1,2 +1,4 @@
-python3 -m pip install --upgrade twine
-python3 -m twine upload dist/gshock_api-2.0.33-py3-none-any.whl dist/gshock_api-2.0.32.tar.gz
+
+uv add --upgrade twine
+uv build
+uv run twine upload -r pypi dist/*
