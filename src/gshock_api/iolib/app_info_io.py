7270595990e267437d64c2b338823c04d7880b71
Ivo Zivkov <izivkov@gmail.com>
2025-11-30 10:35:16 -0500

    Make a fix in AppInfoIO to save app info after reset

--- src/gshock_api/iolib/app_info_io.py
+++ src/gshock_api/iolib/app_info_io.py
@@ -12,7 +12,7 @@ class ConnectionProtocol(Protocol):
     async def request(self, code: str) -> None:
         ...
 
-    def write(self, handle: int, data: bytes | str) -> None:
+    async def write(self, handle: int, data: bytes | str) -> None:
         ...
 
 
@@ -33,15 +33,17 @@ class AppInfoIO:
 
     @staticmethod
     def on_received(data: bytes) -> None:
-        def set_app_info(data_str: str) -> None:
+        async def set_app_info(data_str: str) -> None:
             # App info needed to re-enable button D after reset/CLEARED BLE.
             app_info_compact_str = to_compact_string(data_str)
             if app_info_compact_str == "22FFFFFFFFFFFFFFFFFFFF00":
                 if AppInfoIO.connection is None:
                     raise RuntimeError("AppInfoIO.connection is not set")
-                AppInfoIO.connection.write(0xE, "223488F4E5D5AFC829E06D02")
+                await AppInfoIO.connection.write(0xE, "223488F4E5D5AFC829E06D02")
+            
+            if AppInfoIO.result is None:
+                raise RuntimeError("AppInfoIO.result is not set")
+            AppInfoIO.result.set_result("OK")
 
-        set_app_info(to_hex_string(data))
-        if AppInfoIO.result is None:
-            raise RuntimeError("AppInfoIO.result is not set")
-        AppInfoIO.result.set_result("OK")
+        import asyncio
+        asyncio.create_task(set_app_info(to_hex_string(data)))
